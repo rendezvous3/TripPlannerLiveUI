15d2a9981c33ef9deee06273b9068003f3acee0e
Bojan Jovanovic <bojanj6m@gmail.com>
2017-08-15 17:35:14 -0400

    Added Marker to the map

--- public/bundle.js
+++ public/bundle.js
@@ -542,7 +542,7 @@ const buildMarker = __webpack_require__(4);
   * Instantiate the Map
   */
 
-mapboxgl.accessToken = "---- INSERT YOUR ACCESS TOKEN HERE ----";
+mapboxgl.accessToken = "pk.eyJ1IjoibGNoZXd5IiwiYSI6ImNqNmFnZG1oejB4bnkycHFyc3E5NXRmdjMifQ.QieRY2HH2wA4ApJGqd6enQ";
 const map = new mapboxgl.Map({
   container: "map-canvas",
   center: [-74.0, 40.731],
@@ -551,6 +551,136 @@ const map = new mapboxgl.Map({
   bearing: 20,
   style: "mapbox://styles/mapbox/streets-v10"
 });
+
+fetch('/api')
+  .then(result => result.json())
+  .then(data => {
+    // {hotel: Array(15), restaurant: Array(15), activity: Array(15)}
+    let hotels = data.hotel;
+    let restaurants = data.restaurant;
+    let activites = data.activity;
+    //var counter = 0;
+    hotels.forEach(function(hotel){
+      var optionEl = document.createElement('option');
+      optionEl.append(hotel.name)
+      //optionEl.value = counter.toString();
+      document.getElementById("hotels-choices")
+      .append(optionEl)
+      //counter++
+    })
+
+    restaurants.forEach(function(restaurant){
+      var optionEl = document.createElement('option');
+      optionEl.append(restaurant.name)
+      document.getElementById("restaurants-choices")
+      .append(optionEl)
+    })
+
+   activites.forEach(function(activity){
+      var optionEl = document.createElement('option');
+      optionEl.append(activity.name)
+      document.getElementById("activities-choices")
+      .append(optionEl)
+    })
+
+
+
+  })
+  .catch(console.error);
+
+
+
+//   document.getElementById("hotels-add").click(function(){
+//     console.log("Anything!")
+// })
+
+document.getElementById("hotels-add").addEventListener("click",function(){
+  let e = document.getElementById("hotels-choices");
+  var selectVal = e.value;
+  var listItem = document.createElement('li');
+  var removeBtn = document.createElement('button');
+  removeBtn.className = 'removeBtn btn btn-danger';
+  removeBtn.append("x");
+  listItem.append(selectVal);
+  listItem.appendChild(removeBtn);
+  document.getElementById("hotels-list").appendChild(listItem);
+  fetch('/api')
+  .then(result => result.json())
+  .then(data => {
+    var theHotel;
+    data.hotel.forEach((hotel)=>{
+      if (hotel.name === selectVal){
+        theHotel = hotel;
+      }
+    })
+    return theHotel;
+  })
+  .then(function(hotel){
+    buildMarker("hotels", hotel.place.location).addTo(map);
+  })
+  .catch(console.error);
+  
+  console.log(selectVal)
+})
+
+
+document.getElementById("restaurants-add").addEventListener("click",function(){
+  let e = document.getElementById("restaurants-choices");
+  var selectVal = e.value;
+  var listItem = document.createElement('li');
+  var removeBtn = document.createElement('button');
+  removeBtn.className = 'removeBtn btn btn-danger';
+  removeBtn.append("x");
+  listItem.append(selectVal);
+  listItem.appendChild(removeBtn);
+  document.getElementById("restaurants-list").appendChild(listItem);
+  fetch('/api')
+  .then(result => result.json())
+  .then(data => {
+    var theHotel;
+    data.restaurant.forEach((hotel)=>{
+      if (hotel.name === selectVal){
+        theHotel = hotel;
+      }
+    })
+    return theHotel;
+  })
+  .then(function(hotel){
+    buildMarker("restaurants", hotel.place.location).addTo(map);
+  })
+  .catch(console.error);
+  console.log(selectVal)
+})
+
+document.getElementById("activities-add").addEventListener("click",function(){
+  let e = document.getElementById("activities-choices");
+  var selectVal = e.value;
+  var listItem = document.createElement('li');
+  var removeBtn = document.createElement('button');
+  removeBtn.className = 'removeBtn btn btn-danger';
+  removeBtn.append("x");
+  listItem.append(selectVal);
+  listItem.appendChild(removeBtn);
+  document.getElementById("activities-list").appendChild(listItem);
+  fetch('/api')
+  .then(result => result.json())
+  .then(data => {
+    var theHotel;
+    data.activity.forEach((hotel)=>{
+      if (hotel.name === selectVal){
+        theHotel = hotel;
+      }
+    })
+    return theHotel;
+  })
+  .then(function(hotel){
+    buildMarker("activities", hotel.place.location).addTo(map);
+  })
+  .catch(console.error);
+  console.log(selectVal)
+})
+
+
 
 
 /***/ }),
