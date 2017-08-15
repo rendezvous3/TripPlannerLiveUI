7261728c29c4d165d2a96ca4a5466c56eb20b0d5
Bojan Jovanovic <bojanj6m@gmail.com>
2017-08-15 14:03:50 -0400

    Basic Setup app.js

---
+++ server/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+app.use(function(req, res, next){
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  console.error(err);
+  res.send(
+    // ... fill in this part
+  );
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log('Server is Running!');
+})
